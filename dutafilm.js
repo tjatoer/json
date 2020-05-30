30f0906e146abbaaf72cf0d360467cbfad21b559
tjatoer <catur.budi.raharjo@gmail.com>
2020-05-30 21:34:23 +0700

    Update dutafilm.js

--- dutafilm.js
+++ dutafilm.js
@@ -1,3 +1,5 @@
-document.getElementById("vid-play-icon").style.display = 'none';
-document.getElementById("vid-play-icon").style.visibility = 'hidden';
-//document.getElementById("vid-play-icon").style.visibility = 'visible';
+window.onload = function() {
+  document.getElementById("vid-play-icon").style.display = 'none';
+  document.getElementById("vid-play-icon").style.visibility = 'hidden';
+  //document.getElementById("vid-play-icon").style.visibility = 'visible';
+};
