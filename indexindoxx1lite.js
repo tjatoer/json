e11acbd6c968ee349d78f8d66030f8cbee8ad10a
tjatoer <catur.budi.raharjo@gmail.com>
2021-01-13 19:30:25 +0700

    Create indexindoxx1lite.js

---
+++ indexindoxx1lite.js
@@ -0,0 +1,84 @@
+var admobid = {}
+
+if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
+  admobid = {
+    banner: 'ca-app-pub-3812600009843663/7341878751', //ca-app-pub-3940256099942544/6300978111
+    interstitial: 'ca-app-pub-3812600009843663/2416020983', //ca-app-pub-3940256099942544/1033173712
+    videoreward: 'ca-app-pub-3940256099942544/5224354917',
+  }
+} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
+  admobid = {
+    banner: 'ca-app-pub-3940256099942544/2934735716',
+    interstitial: 'ca-app-pub-3940256099942544/4411468910',
+  }
+} else {
+    admobid = {
+    banner: 'ca-app-pub-3940256099942544/6300978111',
+    interstitial: 'ca-app-pub-3940256099942544/1033173712',
+  }
+}
+
+document.addEventListener('deviceready', function() {
+  admob.banner.config({
+    id: admobid.banner,
+    isTesting: false,
+    autoShow: true,
+  })
+  admob.banner.prepare()
+
+    admob.banner.show()
+
+  admob.interstitial.config({
+    id: admobid.interstitial,
+    isTesting: false,
+    autoShow: false,
+  })
+  admob.interstitial.prepare()
+
+  admob.rewardvideo.config({
+    id: admobid.videoreward,
+    isTesting: false,
+    autoShow: false,
+  })
+  admob.rewardvideo.prepare()
+
+  document.getElementById('showAd').disabled = true
+  document.getElementById('showAd').onclick = function() {
+    admob.interstitial.show()
+  }
+
+
+}, false)
+
+function showInterstitialplay(){
+
+admob.interstitial.show()
+
+}
+
+function showInterstitial(){
+                    admob.interstitial.show()
+                }
+
+function showRewardrewardvideoideo(){
+                    admob.rewardvideo.show()
+                }
+
+document.addEventListener('admob.banner.events.LOAD_FAIL', function(event) {
+  console.log(event)
+})
+
+document.addEventListener('admob.interstitial.events.LOAD_FAIL', function(event) {
+  console.log(event)
+})
+
+document.addEventListener('admob.interstitial.events.LOAD', function(event) {
+  console.log(event)
+  document.getElementById('showAd').disabled = false
+})
+
+document.addEventListener('admob.interstitial.events.CLOSE', function(event) {
+  console.log(event)
+
+  admob.interstitial.prepare()
+})
